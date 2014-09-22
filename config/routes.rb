b661f58488f2e3c471a73ab360e4d15f0c4492bd
Daniel Braithwaite <danielbraithwt@gmail.com>
2014-09-22 21:51:23 +1200

    Updated configuration Updated the configuration of the application

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'calculator/index'
+  #get 'calculator/index'
+  root "calculator#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
